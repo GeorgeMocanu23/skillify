4cf25a3a030e6cc228b4129cdc4c85a5baef8ec9
GeorgeMocanu23 <mocanugeorge90@yahoo.com>
2024-08-29 18:48:44 +0200

    Netlify configuration and environment variables

---
+++ netlify/functions/my-functions.js
@@ -0,0 +1,27 @@
+﻿const { Client } = require('pg');
+
+exports.handler = async function (event, context) {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  try {
+    await client.connect();
+    const res = await client.query('SELECT NOW()');
+    await client.end();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ time: res.rows[0].now }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Database connection failed' }),
+    };
+  }
+};
